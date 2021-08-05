5705222380c4279bf83466871a653e50ed7dcf16
whatsmysuggestion <whatsmysuggestion@gmail.com>
2021-08-05 18:42:26 +0530

    Angular CSS Example

---
+++ src/app/app.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html'  //,
+	/* styles:[
+	
+	'div{ color:red;font-size:25px;}'
+	
+	] */
+ // styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'Hello Welcome';
+}
